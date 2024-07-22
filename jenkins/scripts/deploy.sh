7c005cdc4d23700c8f3e40d5d5b72ef8a02c3525
russelpwq <russelpoon25@gmail.com>
2024-07-23 05:00:25 +0800

    update

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v C:\Users\russe\Downloads\Lab Quiz Materials\Lab7\jenkins-php-selenium-test\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+
+# Ensure paths are correctly formatted for Unix-style paths or use proper escaping
+docker run -d -p 80:80 --name my-apache-php-app -v /c/Users/russe/Downloads/Lab\ Quiz\ Materials/Lab7/jenkins-php-selenium-test/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+
 sleep 1
+
 set +x
 
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
-
