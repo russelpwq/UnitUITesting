e2ac9659354b762d3f7b569178c67fbba627e151
russelpwq <russelpoon25@gmail.com>
2024-07-23 10:22:25 +0800

    Test

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "https://2201319-7096cea5-6071-4f7a-932d-469ea64e220c.socketxp.com";
+	String url = "http://localhost";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
