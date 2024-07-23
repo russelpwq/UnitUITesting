e4b3d79d572376b5b0c62a06d86456c3b37b30b9
russelpwq <russelpoon25@gmail.com>
2024-07-23 10:38:59 +0800

    test

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -3,7 +3,7 @@
 set -x
 
 # Ensure paths are correctly formatted for Unix-style paths or use proper escaping
-docker run -d -p 80:80 --name test-app -v /c/Users/russe/Downloads/Lab\ Quiz\ Materials/Lab7/jenkins-php-selenium-test/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name test-app -v "/c/Users/russe/Downloads/Lab Quiz Materials/Lab7/jenkins-php-selenium-test/jenkins-php-selenium-test/src:/var/www/html" php:7.2-apache
 
 sleep 1
 
